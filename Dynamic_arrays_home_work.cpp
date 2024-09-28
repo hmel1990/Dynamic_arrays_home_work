201a8633c3ebd6c0e691fbdbf826b917223291ce
Maksym <hmel408757595@outlook.com>
2024-09-28 13:11:14 +0300

    Добавьте файлы проекта.

---
+++ Dynamic_arrays_home_work.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int width = 10; 
+	cin >> width;
+	int height = 4;
+	cin >> height;
+	int** array = new int* [height]; // создание указателя на массив указателей
+
+	for (int i = 0; i < height; i++)
+	{
+		array[i] = new int [width]; //выделение памяти для массивов
+	}
+	for (int i = 0; i < height; i++) 
+	{
+		for (int j = 0; j < width; j++) 
+		{
+		array[i][j] = 10;
+		cout << array[i][j] << "  ";
+		}
+	cout << "\n\n";
+}
+}
+
